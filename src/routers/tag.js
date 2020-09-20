928541662b9980795059a06a9112681759624bb0
Ido Hayun <eidoil32@gmail.com>
2020-09-20 16:51:02 +0300

    fix add new tags to recipe

--- src/routers/tag.js
+++ src/routers/tag.js
@@ -5,34 +5,32 @@ const auth = require('../middlefunctions/auth')
 const { create } = require('../models/recipetagconnection')
 const router = new express.Router()
 
-router.post('/api/user/recipe/:recipe_id/create/tags',auth, async(req,res)=>{
-   
+router.post('/api/user/recipe/:recipe_id/create/tags', auth, async(req, res)=>{
     const recipe_id = req.params.recipe_id
-    console.log(req.body.length);
+    const tags = JSON.parse(req.body.tags);
 
-    for(let i=0;i<req.body.length;i++){
-        
+    for (let i = 0; i < tags.length; i++) {
         try {
-            let tag = await Tag.findTagByName(req.body[i].title)
-            if(!tag){
-                tag = new Tag({title:req.body[i].title})
-                await tag.save()
+            let tag = await Tag.findTagByName(tags[i].title);
+            if (!tag) {
+                tag = new Tag({title: tags[i].title});
+                await tag.save();
             }
              
             const recipeTagConnection = new RecipeTagConnection({
-                recipe:recipe_id,
-                tag:tag._id
-            })
-            
-             await recipeTagConnection.save()
-            
-
+                recipe: recipe_id,
+                tag: tag._id
+            });
+        
+            await recipeTagConnection.save();
         } catch (e) {
-         return res.status(400).send(e)
+            return res.status(400).send(e);
         }
     }
-    res.status(201).send('succes')
-})
+
+    res.status(200).send(tags)
+});
+
 router.get('/api/community/tags',auth,async(req,res)=>{
     try {
         const tags = await Tag.find({})
